c8d30c01286dc0a1905a3d89be5233835453179d
gjt <2315275653@qq.com>
2023-06-16 21:51:56 +0800

    "第si次"

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -3,7 +3,7 @@ package org.example;
 public class Main {
     public static void main(String[] args) {
 
-        System.out.println("Hello world!");
+        System.out.println("Hello world master!");
         System.out.println("你好2");
         System.out.println("你哈额哦2 ");
     }
